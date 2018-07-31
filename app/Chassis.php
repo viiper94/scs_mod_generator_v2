355c4ac0b4f3c7f50da5bdd3eca806d11703fcd4
viiper94 <viiper94@gmail.com>
2018-07-31 18:44:34 +0300

    add models

---
+++ app/Chassis.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Chassis extends Model{
+
+    protected $table = 'chassis';
+
+
+}
---
+++ app/Dlc.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Dlc extends Model{
+
+    protected $table = 'dlc';
+
+}
