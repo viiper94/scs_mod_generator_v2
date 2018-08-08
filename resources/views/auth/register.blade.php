7cb042437d5dba157c097e1ceff687225c0e46be
viiper94 <viiper94@gmail.com>
2018-08-08 14:43:02 +0300

    added registration page and stuff

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,61 @@
 @extends('layout.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+<div class="container flex-center" style="flex: 1; align-items: center; flex-direction: column;">
+    <div class="card" style="max-width: 450px;">
+        <form method="POST" action="{{route('register')}}">
+            @csrf
+            <div class="card-content">
+                <div class="row"><h5 class="card-title center">@lang('user.register')</h5></div>
+                <div class="row">
+                    <div class="input-field col s12 no-margin">
+                        <i class="material-icons prefix">person_outline</i>
+                        <input id="name" type="text" name="name" @if($errors->has('name'))class="invalid" @endif value="{{ old('name') }}" required>
+                        <label for="name">@lang('user.name')</label>
+                        @if($errors->has('name'))
+                            <span class="helper-text" data-error="{{ $errors->first('name') }}" data-success=""></span>
+                        @endif
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12 no-margin">
+                        <i class="material-icons prefix">mail_outline</i>
+                        <input id="email" type="email" name="email" @if($errors->has('email'))class="invalid" @endif value="{{ old('email') }}" required>
+                        <label for="email">@lang('user.email')</label>
+                        @if($errors->has('email'))
+                            <span class="helper-text" data-error="{{ $errors->first('email') }}" data-success=""></span>
+                        @endif
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12 no-margin">
+                        <i class="material-icons prefix">lock_outline</i>
+                        <input id="password" type="password" name="password" @if($errors->has('password'))class="invalid" @endif required>
+                        <label for="password">@lang('user.password')</label>
+                        @if($errors->has('password'))
+                            <span class="helper-text" data-error="{{ $errors->first('password') }}" data-success=""></span>
+                        @endif
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12 no-margin">
+                        <i class="material-icons prefix">lock_outline</i>
+                        <input id="password_confirmation" type="password" name="password_confirmation" required>
+                        <label for="password_confirmation">@lang('user.confirm_password')</label>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div class="card-action">
+                <div class="row no-margin">
+                    <button type="submit" class="btn waves-effect col s12 yellow darken-1 black-text"><b>@lang('user.register_now')</b></button>
+                    <div class="input-field col s12">
+                        <p class="center">@lang('user.already_have_account') <a href="{{ route('login') }}">@lang('user.login')</a></p>
+                    </div>
+                </div>
+            </div>
+        </form>
     </div>
 </div>
+
 @endsection
