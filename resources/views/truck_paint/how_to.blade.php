437f8b0b28c558b8b708f7eb8b244f3e6e46509a
viiper94 <viiper94@gmail.com>
2018-08-22 16:37:33 +0300

    added truck paint generator instruction

---
+++ resources/views/truck_paint/how_to.blade.php
@@ -0,0 +1,37 @@
+<aside id="mdc-dialog-how-to" class="mdc-dialog" role="alertdialog" aria-labelledby="my-mdc-dialog-label" aria-describedby="my-mdc-dialog-description">
+    <div class="mdc-dialog__surface">
+        <header class="mdc-dialog__header">
+            <h2 id="my-mdc-dialog-label" class="mdc-dialog__header__title">@lang('instructions.paint_title')</h2>
+        </header>
+        <section id="my-mdc-dialog-description" class="mdc-dialog__body">
+            <h6>@lang('instructions.paint_description')</h6>
+            <ol>
+                <li>@lang('instructions.paint_mod_1')</li>
+                <li>@lang('instructions.paint_mod_2')</li>
+                <li>@lang('instructions.paint_mod_3')</li>
+                <li>@lang('instructions.paint_mod_4')</li>
+                <li>@lang('instructions.paint_mod_5')</li>
+            </ol>
+        </section>
+        <header class="mdc-dialog__header">
+            <h2 id="my-mdc-dialog-label" class="mdc-dialog__header__title">@lang('instructions.install_title')</h2>
+        </header>
+        <section id="my-mdc-dialog-description" class="mdc-dialog__body">
+            <ol>
+                <li>@lang('instructions.install_mod_1')</li>
+                <li>@lang('instructions.install_mod_2')</li>
+                <li>@lang('instructions.install_mod_3')</li>
+                <li>@lang('instructions.install_mod_4')</li>
+                <li>@lang('instructions.install_mod_5')</li>
+                <li>@lang('instructions.install_paint_mod_6')</li>
+                <li>@lang('instructions.install_paint_mod_7')</li>
+                <li>@lang('instructions.install_paint_mod_8')</li>
+                <li>@lang('instructions.install_paint_mod_9')</li>
+            </ol>
+        </section>
+        <footer class="mdc-dialog__footer">
+            <button type="button" class="mdc-button mdc-ripple mdc-button--raised mdc-dialog__footer__button mdc-dialog__footer__button--accept">@lang('instructions.close_modal')</button>
+        </footer>
+    </div>
+    <div class="mdc-dialog__backdrop"></div>
+</aside>
