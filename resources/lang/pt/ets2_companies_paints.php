f620c0849f68c8bccda7f20477854111bb8be670
viiper94 <viiper94@gmail.com>
2018-08-23 12:03:12 +0300

    added languages

---
+++ resources/lang/pt/ets2_companies_paints.php
@@ -0,0 +1,5 @@
+<?php
+
+$return = json_decode(file_get_contents(resource_path('lang/json/'.basename(__DIR__).'.json')), true);
+
+return key_exists('ets2 companies/paints', $return) ? $return['ets2 companies/paints'] : false;
