b370737d1674c0e3656d12c774f79a1d1032c249
viiper94 <viiper94@gmail.com>
2018-08-28 18:02:29 +0300

    hotfix

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -20,7 +20,7 @@
         <li @if(Request::is('gallery'))class="active"@endif>
             <a class="waves-effect" href="{{route('gallery')}}"><i class="material-icons notranslate">photo_library</i>@lang('general.trailers_gallery')</a>
         </li>
-        @if(Auth::user()->admin === 1)
+        @if(Auth::check() && Auth::user()->admin === 1)
             <li>
                 <a class="waves-effect" href="{{route('admin')}}">
                     <i class="material-icons notranslate">developer_mode</i>@lang('admin.admin_title')
