276cef69396b49b2779010ee20871dab907a12b2
viiper94 <viiper94@gmail.com>
2018-09-07 14:22:24 +0300

    hotfix

--- app/Http/Controllers/TruckPaintGeneratorController.php
+++ app/Http/Controllers/TruckPaintGeneratorController.php
@@ -15,13 +15,11 @@ use Illuminate\Support\Facades\Auth;
 class TruckPaintGeneratorController extends Controller{
 
     public function index($game = 'ets2'){
-        $errors = array();
         return view('truck_paint.index', [
             'chassis_list' => Chassis::where(['with_paint_job' => '1', 'game' => 'ets2'])->with('dlc')->get(),
             'game' => $game,
             'wheels' => Wheel::where(['active' => 1, 'game' => $game])->get(),
-            'dlc_list' => Dlc::where(['active' => 1, 'game' => $game])->get(),
-            'errors' => $errors
+            'dlc_list' => Dlc::where(['active' => 1, 'game' => $game])->get()
         ]);
     }
 
