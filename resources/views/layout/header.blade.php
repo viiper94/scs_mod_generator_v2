649e25cb68f2a9720c1d888898c5b16600e3160c
viiper94 <viiper94@gmail.com>
2018-09-10 11:48:34 +0300

    updated materializecss to 1.0

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -2,5 +2,5 @@
 <link type="text/css" rel="stylesheet" href="{{ asset('assets/mdc/css/material-components-web.min.css') }}" media="screen,projection"/>
 <link type="text/css" rel="stylesheet" href="{{ asset('assets/semanticui/semanticui.min.css') }}" media="screen,projection"/>
 <link type="text/css" rel="stylesheet" href="{{ asset('assets/semanticui/semantic-custom.min.css') }}" media="screen,projection"/>
-<link type="text/css" rel="stylesheet" href="{{ asset('assets/materialize/css/materialize.min.css?2805') }}" media="screen,projection"/>
+<link type="text/css" rel="stylesheet" href="{{ asset('assets/materialize/css/materialize.min.css?1009') }}" media="screen,projection"/>
 <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/style.css') }}" media="screen,projection"/>
