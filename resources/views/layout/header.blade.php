86b7a1c0261bd3c9a4e255617178c330249c95d7
viiper94 <viiper94@gmail.com>
2018-09-14 16:24:46 +0300

    add analytics script

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -4,3 +4,13 @@
 <link type="text/css" rel="stylesheet" href="{{ asset('assets/semanticui/semantic-custom.min.css') }}" media="screen,projection"/>
 <link type="text/css" rel="stylesheet" href="{{ asset('assets/materialize/css/materialize.min.css?1009') }}" media="screen,projection"/>
 <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/style.css') }}" media="screen,projection"/>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-99570317-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-99570317-3');
+</script>
