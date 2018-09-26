a05c44972f8a736c3bb28344b85df85a520a8312
viiper94 <viiper94@gmail.com>
2018-09-26 12:03:33 +0300

    update google analytics script

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -5,12 +5,11 @@
 <link type="text/css" rel="stylesheet" href="{{ asset('assets/materialize/css/materialize.min.css?1009') }}" media="screen,projection"/>
 <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/style.css') }}" media="screen,projection"/>
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-99570317-3"></script>
+<!-- Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-99570317-2"></script>
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-99570317-3');
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-99570317-2');
 </script>
