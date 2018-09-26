2235ada61d4b1686ad9a04ba2ad36a4be0668a53
viiper94 <viiper94@gmail.com>
2018-09-26 12:03:16 +0300

    removed beta notification

--- resources/views/generator/index.blade.php
+++ resources/views/generator/index.blade.php
@@ -202,12 +202,6 @@
         @if(!$hasUserAcceptLanguage)
             @include('generator.translate')
         @endif
-        <section class="card-panel grey-text trailer-generator">
-            <span class="card-title">
-                <i class="material-icons left notranslate">warning</i>
-                @lang('general.beta_notification')
-            </span>
-        </section>
     </div>
     <div class="fixed-action-btn tooltipped" data-tooltip="@lang('general.how_to')">
         <a class="mdc-fab mdc-ripple modal-trigger" href="#how" id="how-to">
