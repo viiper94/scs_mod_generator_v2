083ef45828a27f8d1698287287c4ca93d4213992
viiper94 <viiper94@gmail.com>
2018-11-20 15:39:23 +0200

    removed elforastero transliterate

--- config/app.php
+++ config/app.php
@@ -161,7 +161,6 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Invisnik\LaravelSteamAuth\SteamServiceProvider::class,
-        ElForastero\Transliterate\TransliterationServiceProvider::class,
 
     ],
 
@@ -211,8 +210,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
-        'Transliterate' => ElForastero\Transliterate\TransliterationFacade::class,
 
     ],
 
