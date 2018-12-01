b7ea554a5d17a39b80b0041cb14a5a0d44e6d02a
viiper94 <viiper94@gmail.com>
2018-12-01 17:23:03 +0200

    update version

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -18,7 +18,7 @@
     </div>
     <div class="footer-copyright">
         <div class="container">
-            <span class="left">@lang('general.current_version'): 2.2.0</span>
+            <span class="left">@lang('general.current_version'): 2.2.1</span>
             <span class="right">&copy; <a href="http://steamcommunity.com/id/viiper94/" target="_blank">Mayday</a> - {{date('Y')}}</span>
         </div>
     </div>
