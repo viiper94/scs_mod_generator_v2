c2591ff6d3ae23a606b27d7099506dd9f6c1362b
viiper94 <viiper94@gmail.com>
2019-01-14 00:02:57 +0200

    added discord server

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -9,7 +9,8 @@
             <div class="col m4 s12 right-align">
                 <h6>@lang('general.links')</h6>
                 <ul>
-                    <li><a href="https://forum.truckersmp.com/index.php?/topic/59888-trailer-mod-web-generator/" target="_blank">@lang('general.forums_topic')</a></li>
+                    <li><a href="https://forum.truckersmp.com/index.php?/topic/59888-trailer-mod-web-generator" target="_blank">@lang('general.forums_topic')</a></li>
+                    <li><a href="https://discord.gg/EjaN9j" target="_blank">@lang('general.discord')</a></li>
                     <li><a href="https://truckersmp.com" target="_blank">@lang('general.truckersmp')</a></li>
                     <li><a href="http://blog.scssoft.com" target="_blank">@lang('general.scs_blog')</a></li>
                 </ul>
@@ -18,7 +19,7 @@
     </div>
     <div class="footer-copyright">
         <div class="container">
-            <span class="left">@lang('general.current_version'): 2.2.4</span>
+            <span class="left">@lang('general.current_version'): 2.2.5</span>
             <span class="right">&copy; <a href="http://steamcommunity.com/id/viiper94/" target="_blank">Mayday</a> - {{date('Y')}}</span>
         </div>
     </div>
