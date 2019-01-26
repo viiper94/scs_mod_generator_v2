d8d21825198ac20ad4410e7c37c26ef13a8cd118
viiper94 <viiper94@gmail.com>
2019-01-26 22:03:33 +0200

    update discord invite

--- resources/views/layout/discord.blade.php
+++ resources/views/layout/discord.blade.php
@@ -2,8 +2,8 @@
     <span class="card-title">
         <i class="material-icons left notranslate">info</i>
         @lang('general.join_discord')
-        <a href="https://discord.gg/EjaN9j"
+        <a href="https://discord.gg/JtQkAAV"
            target="_blank"
-           class="grey-text text-darken-1" style="text-decoration: underline; white-space: nowrap;">https://discord.gg/EjaN9j</a>
+           class="grey-text text-darken-1" style="text-decoration: underline; white-space: nowrap;">https://discord.gg/JtQkAAV</a>
     </span>
 </section>
