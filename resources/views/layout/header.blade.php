041a490907bd2316a8e1ad8ad9c6e2838e6b93e5
viiper94 <viiper94@gmail.com>
2019-01-30 19:46:42 +0200

    using webpack for generating assets

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,9 +1,5 @@
 <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
-<link type="text/css" rel="stylesheet" href="{{ asset('assets/mdc/css/material-components-web.min.css') }}" media="screen,projection"/>
-<link type="text/css" rel="stylesheet" href="{{ asset('assets/semanticui/semanticui.min.css') }}" media="screen,projection"/>
-<link type="text/css" rel="stylesheet" href="{{ asset('assets/semanticui/semantic-custom.min.css') }}" media="screen,projection"/>
-<link type="text/css" rel="stylesheet" href="{{ asset('assets/materialize/css/materialize.min.css?1009') }}" media="screen,projection"/>
-<link type="text/css" rel="stylesheet" href="{{ asset('assets/css/style-2.2.12.min.css') }}" media="screen,projection"/>
+<link type="text/css" rel="stylesheet" href="{{ mix('css/app.css') }}" media="screen,projection"/>
 <link rel="manifest" href="{{ asset('manifest.json') }}">
 
 <!-- Google Analytics -->
