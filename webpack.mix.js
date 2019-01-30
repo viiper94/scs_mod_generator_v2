4d3b39206b21d0296ef4391dda11d2cd53b64d47
viiper94 <viiper94@gmail.com>
2019-01-30 19:47:09 +0200

    using webpack for generating assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,12 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.sass('resources/assets/sass/app.scss', 'public/css').version()
+    .combine([
+        'resources/assets/js/jquery-3.2.1.min.js',
+        'resources/assets/js/mdc/material-components-web.js',
+        'resources/assets/js/semanticui/semanticui.min.js',
+        'resources/assets/js/materialize/materialize.js',
+        'resources/assets/js/script.js',
+    ], 'public/js/app.js')
+    .version();
