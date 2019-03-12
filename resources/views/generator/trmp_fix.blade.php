8398147140ca7c89b56e056191da2bb3e14eb238
viiper94 <viiper94@gmail.com>
2019-03-12 11:36:34 +0200

    moved tmp_fix to advanced section

--- resources/views/generator/trmp_fix.blade.php
+++ resources/views/generator/trmp_fix.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <p class="col s12">
+    <p>
         <label>
             <input type="checkbox" class="filled-in" checked="checked" name="fix">
             <span>@lang('general.include_fix')</span>
