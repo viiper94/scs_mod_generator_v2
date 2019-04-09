0255bb7cb3a57229a6fd52efe6fb0f03f8e1680c
viiper94 <viiper94@gmail.com>
2019-04-09 14:47:19 +0300

    updated MDC markup

--- resources/views/layout/scripts.blade.php
+++ resources/views/layout/scripts.blade.php
@@ -1,5 +1,4 @@
 <script type="text/javascript" src="{{ asset('assets/js/jquery-3.2.1.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('assets/mdc/js/material-components-web.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('assets/semanticui/semanticui.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('assets/materialize/js/materialize.min.js?1009') }}"></script>
 <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
