350db9fa5c8a815797f2a811375e51080fa00e3f
viiper94 <viiper94@gmail.com>
2019-04-09 14:44:54 +0300

    added top app bar

--- resources/views/admin/layout/navbar.blade.php
+++ resources/views/admin/layout/navbar.blade.php
@@ -1,61 +1,79 @@
-<nav id="slide-out" class="sidenav sidenav-fixed" style="text-transform: uppercase; width: 350px;">
-    <ul class="top-menu">
-        <li><a class="waves-effect" href="{{url('/')}}"><i class="material-icons notranslate">settings_backup_restore</i>До генератора</a></li>
-        {{--<li @if(Request::is('admin'))class="active"@endif>--}}
-            {{--<a class="waves-effect" href="{{route('admin')}}"><i class="material-icons notranslate">home</i>Головна</a>--}}
-        {{--</li>--}}
-        <li @if(Request::is('admin/trailers'))class="active"@endif>
-            <a class="waves-effect" href="{{route('trailers')}}"><i class="material-icons notranslate">local_shipping</i>Шассі</a>
-        </li>
-        <li @if(Request::is('admin/accessories'))class="active"@endif>
-            <a class="waves-effect" href="{{route('accessories')}}"><i class="material-icons notranslate">category</i>Аксесуари</a>
-        </li>
-        <li @if(Request::is('admin/paints'))class="active"@endif>
-            <a class="waves-effect" href="{{route('paints')}}"><i class="material-icons notranslate">texture</i>Скіни</a>
-        </li>
-        <li @if(Request::is('admin/companies'))class="active"@endif>
-            <a class="waves-effect" href="{{route('companies')}}"><i class="material-icons notranslate">account_balance</i>Компанії</a>
-        </li>
-        <li @if(Request::is('admin/wheels'))class="active"@endif>
-            <a class="waves-effect" href="{{route('wheels')}}"><i class="material-icons notranslate">adjust</i>Колеса</a>
-        </li>
-        <li @if(Request::is('admin/dlc'))class="active"@endif>
-            <a class="waves-effect" href="{{route('dlc')}}"><i class="material-icons notranslate">extension</i>DLC</a>
-        </li>
-        <li @if(Request::is('admin/mods'))class="active"@endif>
-            <a class="waves-effect" href="{{route('mods')}}"><i class="material-icons notranslate">build</i>Модифікації</a>
-        </li>
-        <li @if(Request::is('admin/static_mods'))class="active"@endif>
-            <a class="waves-effect" href="{{route('admin_static_mods')}}"><i class="material-icons notranslate">build</i>Статичні модифікації</a>
-        </li>
-        <li @if(Request::is('admin/languages'))class="active"@endif>
-            <a class="waves-effect" href="{{route('languages')}}"><i class="material-icons notranslate">language</i>Мови</a>
-        </li>
-        <li @if(Request::is('admin/users'))class="active"@endif>
-            <a class="waves-effect" href="{{route('users')}}"><i class="material-icons notranslate">people</i>Користувачі</a>
-        </li>
-    </ul>
-    <ul class="bottom-menu">
-        <li class="divider" style="margin: 0 32px; background-color: #9b9b9b;"></li>
-        <li @if(Request::is('profile'))class="active"@endif>
-            <a class="waves-effect" href="{{route('profile')}}"><i class="material-icons notranslate">person</i>@lang('user.profile')</a>
-        </li>
-        <li>
-            <a class="waves-effect" href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                <i class="material-icons notranslate">exit_to_app</i>@lang('user.logout')
+<div class="navbar-fixed">
+    <nav class="nav-extended no-shadow">
+        <div class="nav-left">
+            <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons notranslate">menu</i></a>
+            <a href="/" class="logo"></a>
+        </div>
+        @yield('navbar-content')
+        <div class="nav-right">
+            <a class="tooltipped material-icons notranslate mdc-icon-button" id="toggle-dark"
+               data-tooltip="@lang('general.dark_theme')" data-position="bottom">
+                @if(isset($_COOKIE['dark_theme']) && $_COOKIE['dark_theme'] == 'true')brightness_high
+                @else brightness_low
+                @endif
             </a>
-            <form id="logout-form" action="{{route('logout')}}" method="POST" style="display: none;">
-                @csrf
-            </form>
-        </li>
-        <li style="padding: 0 32px;">
-            <div class="mdc-switch" style="margin-right: 18px;">
-                <input type="checkbox" id="toggle-dark" class="mdc-switch__native-control" @if(isset($_COOKIE['dark_theme']) && $_COOKIE['dark_theme'] == 'true') checked @endif>
-                <div class="mdc-switch__background">
-                    <div class="mdc-switch__knob"></div>
+            <a href="#" class="user-view" data-target="profile-actions">
+                <img class="circle" src="{{ '/images/users/'.Auth::user()->image }}">
+            </a>
+            <div class="dropdown-content" id="profile-actions">
+                <div class="flex user-info">
+                    <a href="{{ route('profile') }}"><img class="circle user-image" src="{{ '/images/users/'.Auth::user()->image }}"></a>
+                    <div class="menu-right">
+                        <span class="user-name">{{ Auth::user()->name }}</span>
+                        <span class="user-email">{{ Auth::user()->email }}</span>
+                    </div>
+                </div>
+                <div class="user-actions">
+                    <a href="{{ route('profile_edit') }}" class="mdc-button mdc-button--outlined">@lang('user.profile_edit')</a>
+                    <a href="{{ route('logout') }}" class="mdc-button mdc-button--outlined"
+                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">@lang('user.logout')</a>
+                    <form id="logout-form" action="{{route('logout')}}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
                 </div>
             </div>
-            <label for="toggle-dark" class="mdc-switch-label" style="font-weight: 500;">@lang('general.dark_theme')</label>
+        </div>
+    </nav>
+</div>
+
+<nav id="slide-out" class="sidenav sidenav-fixed no-shadow">
+    <ul>
+        <li><a class="waves-effect" href="{{url('/')}}"><i class="material-icons notranslate">settings_backup_restore</i>До генератора</a></li>
+        <li class="divider"></li>
+        {{--<li @if(Request::is('admin'))class="active"@endif>--}}
+            {{--<a href="{{route('admin')}}"><i class="material-icons notranslate">home</i>Головна</a>--}}
+        {{--</li>--}}
+        <li @if(Request::is('admin/trailers'))class="active"@endif>
+            <a href="{{route('trailers')}}"><i class="material-icons notranslate">local_shipping</i>Шассі</a>
+        </li>
+        <li @if(Request::is('admin/accessories'))class="active"@endif>
+            <a href="{{route('accessories')}}"><i class="material-icons notranslate">category</i>Аксесуари</a>
+        </li>
+        <li @if(Request::is('admin/paints'))class="active"@endif>
+            <a href="{{route('paints')}}"><i class="material-icons notranslate">texture</i>Скіни</a>
+        </li>
+        <li @if(Request::is('admin/companies'))class="active"@endif>
+            <a href="{{route('companies')}}"><i class="material-icons notranslate">account_balance</i>Компанії</a>
+        </li>
+        <li @if(Request::is('admin/wheels'))class="active"@endif>
+            <a href="{{route('wheels')}}"><i class="material-icons notranslate">adjust</i>Колеса</a>
+        </li>
+        <li @if(Request::is('admin/dlc'))class="active"@endif>
+            <a href="{{route('dlc')}}"><i class="material-icons notranslate">extension</i>DLC</a>
+        </li>
+        <li class="divider"></li>
+        <li @if(Request::is('admin/mods'))class="active"@endif>
+            <a href="{{route('mods')}}"><i class="material-icons notranslate">build</i>Модифікації</a>
+        </li>
+        <li @if(Request::is('admin/static_mods'))class="active"@endif>
+            <a href="{{route('admin_static_mods')}}"><i class="material-icons notranslate">build</i>Статичні модифікації</a>
+        </li>
+        <li class="divider"></li>
+        <li @if(Request::is('admin/languages'))class="active"@endif>
+            <a href="{{route('languages')}}"><i class="material-icons notranslate">language</i>Мови</a>
+        </li>
+        <li @if(Request::is('admin/users'))class="active"@endif>
+            <a href="{{route('users')}}"><i class="material-icons notranslate">people</i>Користувачі</a>
         </li>
     </ul>
 </nav>
--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,31 +1,69 @@
-<nav id="slide-out" class="sidenav sidenav-fixed">
-    <ul class="top-menu">
-        @if(Auth::check())
-            <li>
-                <div class="user-view" style="min-height: 100px;">
-                    <a href="{{ route('profile') }}">
-                        @if(Auth::user()->image && file_exists(public_path('images/users/'.Auth::user()->image)))
-                            <img class="circle" src="{{ '/images/users/'.Auth::user()->image }}">
-                        @else
-                            <img class="circle" src="{{ '/images/users/default.jpg' }}">
-                        @endif
-                        <span class="name center">{{ Auth::user()->name }}</span></a>
+<div class="navbar-fixed">
+    <nav class="nav-extended no-shadow">
+        <div class="nav-left">
+            <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons notranslate">menu</i></a>
+            <a href="/" class="logo"></a>
+        </div>
+        @yield('navbar-content')
+        <div class="nav-right">
+            <a class="tooltipped material-icons notranslate mdc-icon-button" id="toggle-dark"
+               data-tooltip="@lang('general.dark_theme')" data-position="bottom">
+                @if(isset($_COOKIE['dark_theme']) && $_COOKIE['dark_theme'] == 'true')brightness_high
+                @else brightness_low
+                @endif
+            </a>
+            <a class="tooltipped material-icons notranslate mdc-icon-button" href="#lang" id="lang-sw"
+               data-tooltip="@lang('general.languages')" data-position="bottom">language</a>
+            @guest
+                <a href="{{ route('login') }}" class="mdc-button mdc-button--unelevated">@lang('user.login')</a>
+            @else
+                <a href="#" class="user-view" data-target="profile-actions">
+                    <img class="circle" src="{{ '/images/users/'.Auth::user()->image }}">
+                </a>
+                <div class="dropdown-content" id="profile-actions">
+                    <div class="flex user-info">
+                        <a href="{{ route('profile') }}"><img class="circle user-image" src="{{ '/images/users/'.Auth::user()->image }}"></a>
+                        <div class="menu-right">
+                            <span class="user-name">{{ Auth::user()->name }}</span>
+                            <span class="user-email">{{ Auth::user()->email }}</span>
+                            <a href="{{ route('profile') }}" style="width: 100%;"
+                               class="mdc-button mdc-button--dense mdc-button--unelevated">
+                                <i class="material-icons mdc-button__icon notranslate">perm_identity_filled</i>
+                                @lang('user.profile')
+                            </a>
+                        </div>
+                    </div>
+                    <div class="user-actions">
+                        <a href="{{ route('profile_edit') }}" class="mdc-button mdc-button--outlined">@lang('user.profile_edit')</a>
+                        <a href="{{ route('logout') }}" class="mdc-button mdc-button--outlined"
+                           onclick="event.preventDefault(); document.getElementById('logout-form').submit();">@lang('user.logout')</a>
+                        <form id="logout-form" action="{{route('logout')}}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
                 </div>
-            </li>
-        @endif
+            @endguest
+        </div>
+    </nav>
+</div>
+
+<nav id="slide-out" class="sidenav sidenav-fixed no-shadow">
+    <ul style="flex: 1">
         <li @if(Request::is('/'))class="active"@endif>
-            <a class="waves-effect" href="{{url('/')}}"><i class="truck-icons truck-eu"></i>@lang('general.ets2') @lang('general.trailer_generator')</a>
+            <a href="{{url('/')}}"><i class="truck-icons truck-eu"></i>@lang('general.ets2') @lang('general.trailer_generator')</a>
         </li>
         <li @if(Request::is('ats'))class="active"@endif>
-            <a class="waves-effect" href="{{url('/', 'ats')}}"><i class="truck-icons truck-us"></i>@lang('general.ats') @lang('general.trailer_generator')</a>
+            <a href="{{url('/', 'ats')}}"><i class="truck-icons truck-us"></i>@lang('general.ats') @lang('general.trailer_generator')</a>
         </li>
         <li @if(Request::is('color'))class="active"@endif>
-            <a class="waves-effect" href="{{route('color')}}"><i class="material-icons notranslate">format_paint</i>@lang('general.truck_paint_job_generator')</a>
+            <a href="{{route('color')}}"><i class="material-icons notranslate">format_paint</i>@lang('general.truck_paint_job_generator')</a>
         </li>
+        <li class="divider"></li>
         <li @if(Request::is('gallery'))class="active"@endif>
-            <a class="waves-effect" href="{{route('gallery')}}"><i class="material-icons notranslate">photo_library</i>@lang('general.trailers_gallery')</a>
+            <a href="{{route('gallery')}}"><i class="material-icons notranslate">photo_library</i>@lang('general.trailers_gallery')</a>
         </li>
-        <li @if(Request::is('mods/ets2') || Request::is('mods/ats'))class="active"@endif>
+        <li class="divider"></li>
+        <li>
             <ul class="collapsible collapsible-accordion">
                 <li @if(Request::is('mods/ets2') || Request::is('mods/ats'))class="active"@endif>
                     <a class="collapsible-header waves-effect"><i class="material-icons notranslate">build</i>@lang('general.static_mods')</a>
@@ -43,45 +81,15 @@
             </ul>
         </li>
         @can('admin')
+            <li class="divider"></li>
             <li>
-                <a class="waves-effect" href="{{route('trailers')}}">
+                <a href="{{route('trailers')}}">
                     <i class="material-icons notranslate">developer_mode</i>@lang('admin.admin_title')
                 </a>
             </li>
         @endcan
     </ul>
-    <ul class="bottom-menu">
-        <li class="divider" style="margin: 0 32px; background-color: #9b9b9b;"></li>
-        @guest
-            <li @if(Request::is('login'))class="active"@endif>
-                <a class="waves-effect" href="{{route('login')}}"><i class="material-icons notranslate">how_to_reg</i>@lang('user.login')</a>
-            </li>
-            <li @if(Request::is('register'))class="active"@endif>
-                <a class="waves-effect" href="{{route('register')}}"><i class="material-icons notranslate">person_add</i>@lang('user.register')</a>
-            </li>
-        @else
-            <li @if(Request::is('profile'))class="active"@endif>
-                <a class="waves-effect" href="{{route('profile')}}"><i class="material-icons notranslate">person</i>@lang('user.profile')</a>
-            </li>
-            <li>
-                <a class="waves-effect" href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                    <i class="material-icons notranslate">exit_to_app</i>@lang('user.logout')
-                </a>
-                <form id="logout-form" action="{{route('logout')}}" method="POST" style="display: none;">
-                    @csrf
-                </form>
-            </li>
-        @endguest
-        <li><a class="waves-effect" href="#lang" id="lang-sw"><i class="material-icons notranslate">language</i>@lang('general.languages')</a></li>
-        <li style="padding: 0 32px;">
-            <div class="mdc-switch" style="margin-right: 18px;">
-                <input type="checkbox" id="toggle-dark" class="mdc-switch__native-control" @if(isset($_COOKIE['dark_theme']) && $_COOKIE['dark_theme'] == 'true') checked @endif>
-                <div class="mdc-switch__background">
-                    <div class="mdc-switch__knob"></div>
-                </div>
-            </div>
-            <label for="toggle-dark" class="mdc-switch-label" style="font-weight: 500;">@lang('general.dark_theme')</label>
-        </li>
-    </ul>
+    <div class="nav-footer">
+        <span><a href="http://steamcommunity.com/id/viiper94/" target="_blank">Mayday</a> - @lang('general.current_version'): <b>3.0</b></span>
+    </div>
 </nav>
-<a href="#" data-target="slide-out" class="sidenav-trigger hide-on-large-only"><i class="material-icons notranslate">menu</i></a>
