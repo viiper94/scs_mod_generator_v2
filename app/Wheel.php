c66c9ff58776d7ce314778e871abb11e2e9be9a0
viiper94 <viiper94@gmail.com>
2019-04-12 18:46:13 +0300

    improved wheels generation

--- app/Wheel.php
+++ app/Wheel.php
@@ -12,6 +12,36 @@ class Wheel extends Model{
         'active' => 'boolean',
     ];
 
+    public static $rules = [
+        0 => [
+            'chemical_long' => [
+                'def' => '/def/vehicle/t_wheel/front.sii',
+            ],
+            'acid_long' => [
+                'def' => '/def/vehicle/t_wheel/front.sii',
+                'required' => true
+            ],
+            'flatbed53_4ax' => [
+                'def' => '/def/vehicle/t_wheel/front.sii',
+            ],
+        ],
+        1 => [],
+        2 => [
+            'schw_overweight' => [
+                'def' => '/def/vehicle/t_wheel/overweight_f.sii',
+                'offset' => '0',
+                'required' => true
+            ]
+        ],
+        3 => [],
+        4 => [],
+        5 => [],
+        6 => [],
+        7 => [],
+        8 => [],
+        9 => [],
+    ];
+
     public function dlc(){
         return $this->belongsTo('App\Dlc');
     }
@@ -25,6 +55,64 @@ class Wheel extends Model{
         if(!$this->mp_support) $name .= '<span class="hint left">'.trans('general.mp_no_support').'</span>';
         if($this->isDLCContent()) $name .= '<span class="hint right">'.trans('dlc_list.'.$this->dlc->name).'</span>';
         return $name;
+    }
+
+    public static function getWheelState($axle, $chassis){
+        /** Some boolean algebra magic here
+         *
+         * A - $hasRule
+         * R - $isRequired
+         * C - $isCustom
+         * S - supportWheels
+         *  _____________________
+         * | A | R | C | S | out |
+         * |---------------------|
+         * | 0 | 0 | 0 | 0 |  1  |
+         * | 0 | 0 | 0 | 1 |  2  |
+         * | 0 | 0 | 1 | 0 |  *  |
+         * | 0 | 0 | 1 | 1 |  1  |
+         * | 0 | 1 | 0 | 0 |  *  |
+         * | 0 | 1 | 0 | 1 |  *  |
+         * | 0 | 1 | 1 | 0 |  *  |
+         * | 0 | 1 | 1 | 1 |  *  |
+         * | 1 | 0 | 0 | 0 |  3  |
+         * | 1 | 0 | 0 | 1 |  2  |
+         * | 1 | 0 | 1 | 0 |  *  |
+         * | 1 | 0 | 1 | 1 |  1  |
+         * | 1 | 1 | 0 | 0 |  3  |
+         * | 1 | 1 | 0 | 1 |  3  |
+         * | 1 | 1 | 1 | 0 |  *  |
+         * | 1 | 1 | 1 | 1 |  3  |
+         * ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+         *
+         *        R      !R
+         *    |¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|
+         *    | 3 | 3 | 2 | 3 | !C
+         *  A |−−−−−−−−−−−−−|
+         *    | * | 3 | 1 | * |
+         *    |−−−−−−−−−−−−−|  C
+         *    | * | * | 1 | * |
+         * !A |−−−−−−−−−−−−−| !C
+         *    | * | * | 2 | 1 |
+         *    ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+         *     !S     S    !S
+         *
+         * 1 = !RCS || !A!R!S
+         * 2 = !R!CS
+         * 3 = R || A!S
+         */
+        $rules = self::$rules;
+
+        $hasRule = key_exists($chassis->alias, $rules[$axle]);
+        $isRequired = $hasRule && isset($rules[$axle][$chassis->alias]['required']) && $rules[$axle][$chassis->alias]['required'];
+        $isCustom = $chassis->customWheels;
+        $supportWheels = $chassis->supports_wheels;
+
+        if((!$isRequired && $isCustom && $supportWheels) || (!$hasRule && !$isRequired && !$supportWheels)) return 1;
+        if(!$isRequired && !$isCustom && $supportWheels) return 2;
+        if($isRequired || ($hasRule && !$supportWheels)) return 3;
+
+        return false;
     }
 
 }
