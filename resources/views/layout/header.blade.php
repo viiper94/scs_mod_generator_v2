57dd74e6360b848af3e4252226744defb0b6d028
viiper94 <viiper94@gmail.com>
2019-04-27 21:47:19 +0300

    updated gtag id

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -2,11 +2,12 @@
 <link type="text/css" rel="stylesheet" href="{{ mix('css/app.css') }}" media="screen,projection"/>
 <link rel="manifest" href="{{ asset('manifest.json') }}">
 
-<!-- Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-99570317-2"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-139146641-1"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', 'UA-99570317-2');
+
+    gtag('config', 'UA-139146641-1');
 </script>
