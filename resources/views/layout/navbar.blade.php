d0e6f0ac9c51e892b52adca0fef1c316fecf5ce9
viiper94 <viiper94@gmail.com>
2019-06-19 20:26:49 +0300

    updated version

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -87,6 +87,6 @@
         @endcan
     </ul>
     <div class="nav-footer">
-        <span><a href="http://steamcommunity.com/id/viiper94/" target="_blank">Mayday</a> - @lang('general.current_version'): <b>3.1</b></span>
+        <span><a href="http://steamcommunity.com/id/viiper94/" target="_blank">Mayday</a> - @lang('general.current_version'): <b>3.1.1</b></span>
     </div>
 </nav>
