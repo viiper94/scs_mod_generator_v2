6fdd4d93e503d6d1832f3cab950e31e926423bee
viiper94 <viiper94@gmail.com>
2019-11-12 23:10:19 +0200

    added ats 1.36 support

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -4,7 +4,7 @@
             <div class="col m8 s12">
                 <h5>@lang('general.supported_versions')</h5>
                 <p class="fs20">@lang('general.ets2') - <b>1.35.*</b></p>
-                <p class="fs20">@lang('general.ats') - <b>1.35.*</b></p>
+                <p class="fs20">@lang('general.ats') - <b>1.36.*</b></p>
             </div>
             <div class="col m4 s12 right-align">
                 <h5>@lang('general.links')</h5>
