0c9758eca757eec4277a4d554a80ded90d980462
Eugene <viiper94@gmail.com>
2020-02-26 22:19:04 +0200

    improved translation

--- resources/lang/ro/accessories.php
+++ resources/lang/ro/accessories.php
@@ -5,7 +5,8 @@ $lang = json_decode(file_get_contents(resource_path('lang/json/'.basename(__DIR_
 $static = json_decode(file_get_contents(resource_path('lang/json/static.json')), true);
 
 $locale_arr = key_exists('accessories', $locale) ? $locale['accessories'] : [];
+$shared_arr = key_exists('shared', $lang) ? $lang['shared'] : [];
 $lang_arr = key_exists('accessories', $lang) ? $lang['accessories'] : [];
 $static_arr = key_exists('accessories', $static) ? $static['accessories'] : [];
 
-return array_merge($locale_arr, $lang_arr, $static_arr);
+return array_merge($locale_arr, $shared_arr, $lang_arr, $static_arr);
--- resources/lang/ru/ets2_companies_paints.php
+++ resources/lang/ru/ets2_companies_paints.php
@@ -4,6 +4,7 @@ $lang = json_decode(file_get_contents(resource_path('lang/json/'.basename(__DIR_
 $static = json_decode(file_get_contents(resource_path('lang/json/static.json')), true);
 
 $lang_arr = key_exists('companies/paints', $lang) ? $lang['companies/paints'] : [];
+$shared_arr = key_exists('shared', $lang) ? $lang['shared'] : [];
 $static_arr = key_exists('companies/paints', $static) ? $static['companies/paints'] : [];
 
-return array_merge($lang_arr, $static_arr);
+return array_merge($shared_arr, $lang_arr, $static_arr);
