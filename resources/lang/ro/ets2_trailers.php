e51b31ed839fa1d8a1387fc6ba210b775c247bfd
Eugene <viiper94@gmail.com>
2020-02-28 16:57:36 +0200

    improved translation

--- resources/lang/ro/ets2_trailers.php
+++ resources/lang/ro/ets2_trailers.php
@@ -4,8 +4,9 @@ $locale = json_decode(file_get_contents(resource_path('lang/locale/'.basename(__
 $lang = json_decode(file_get_contents(resource_path('lang/json/'.basename(__DIR__).'.json')), true);
 $static = json_decode(file_get_contents(resource_path('lang/json/static.json')), true);
 
+$shared_arr = key_exists('shared', $lang) ? $lang['shared'] : [];
 $locale_arr = key_exists('ets2 trailers', $locale) ? $locale['ets2 trailers'] : [];
 $lang_arr = key_exists('ets2 trailers', $lang) ? $lang['ets2 trailers'] : [];
 $static_arr = key_exists('ets2 trailers', $static) ? $static['ets2 trailers'] : [];
 
-return array_merge($locale_arr, $lang_arr, $static_arr);
+return array_merge($shared_arr, $locale_arr, $lang_arr, $static_arr);
