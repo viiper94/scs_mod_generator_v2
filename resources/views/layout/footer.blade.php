6fd318ef2d6d63415bcc73211755af1c2eaa870a
Eugene <viiper94@gmail.com>
2020-07-22 10:16:01 +0300

    added promods supported version

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -3,8 +3,9 @@
         <div class="row">
             <div class="col m8 s12">
                 <h5>@lang('general.supported_versions')</h5>
-                <p class="fs20">@lang('general.ets2') - <b>1.37.*</b></p>
-                <p class="fs20">@lang('general.ats') - <b>1.37.*</b></p>
+                <p class="fs17">@lang('general.ets2') - <b>1.37.*</b></p>
+                <p class="fs17">@lang('general.ats') - <b>1.38.*</b></p>
+                <p class="fs17">@lang('dlc_list.promods') - <b>2.46</b></p>
             </div>
             <div class="col m4 s12 right-align">
                 <h5>@lang('general.links')</h5>
