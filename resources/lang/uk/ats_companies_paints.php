ae80f5890730f725e30a1ec12980658ae986f5de
Eugene <viiper94@gmail.com>
2021-02-13 11:45:48 +0200

    added default and metallic names to locale

--- resources/lang/uk/ats_companies_paints.php
+++ resources/lang/uk/ats_companies_paints.php
@@ -1,10 +1,12 @@
 <?php
 
+$locale = json_decode(file_get_contents(resource_path('lang/locale/'.basename(__DIR__).'.json')), true);
 $lang = json_decode(file_get_contents(resource_path('lang/json/'.basename(__DIR__).'.json')), true);
 $static = json_decode(file_get_contents(resource_path('lang/json/static.json')), true);
 
 $lang_arr = key_exists('companies/paints', $lang) ? $lang['companies/paints'] : [];
 $shared_arr = key_exists('shared', $lang) ? $lang['shared'] : [];
 $static_arr = key_exists('companies/paints', $static) ? $static['companies/paints'] : [];
+$locale_arr = key_exists('companies/paints', $locale) ? $locale['companies/paints'] : [];
 
-return array_merge($shared_arr, $lang_arr, $static_arr);
+return array_merge($shared_arr, $locale_arr, $lang_arr, $static_arr);
