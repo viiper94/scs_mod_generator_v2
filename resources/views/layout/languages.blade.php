4b8fc1b0082f4b6c6442b73b93f4ea9bd9b49357
Eugene <viiper94@gmail.com>
2021-04-13 10:04:53 +0300

    fixed error on loading asset, fixed onesky link protocol

--- resources/views/layout/languages.blade.php
+++ resources/views/layout/languages.blade.php
@@ -10,24 +10,26 @@
             </div>
             <div class="mdc-dialog__content" id="my-dialog-content">
                 <div class="row">
-                    @foreach (array_chunk($languages, ceil(count($languages)/2), true) as $array)
-                        <div class="col m6 s12 lang-col">
-                            @foreach($array as $locale => $lang)
-                                <a href="{{!$_SERVER['QUERY_STRING'] ? '' : '?'.$_SERVER['QUERY_STRING']}}"
-                                   class="valign-wrapper lang-btn @if(\Illuminate\Support\Facades\App::isLocale($locale))active @endif"
-                                   data-lang="{{$locale}}">
-                                    <img src="{{ asset('assets/img/langs/'.$locale.'.png') }}" alt="{{$lang['title']}}>">
-                                    {{$lang['title']}}
-                                </a>
-                            @endforeach
-                        </div>
-                    @endforeach
+                    @if(isset($languages))
+                        @foreach (array_chunk($languages, ceil(count($languages)/2), true) as $array)
+                            <div class="col m6 s12 lang-col">
+                                @foreach($array as $locale => $lang)
+                                    <a href="{{!$_SERVER['QUERY_STRING'] ? '' : '?'.$_SERVER['QUERY_STRING']}}"
+                                       class="valign-wrapper lang-btn @if(\Illuminate\Support\Facades\App::isLocale($locale))active @endif"
+                                       data-lang="{{$locale}}">
+                                        <img src="{{ asset('assets/img/langs/'.$locale.'.png') }}" alt="{{$lang['title']}}>">
+                                        {{$lang['title']}}
+                                    </a>
+                                @endforeach
+                            </div>
+                        @endforeach
+                    @endif
                 </div>
                 <div class="row">
                     <h6 class="center" style="font-size: 14px">@lang('general.help_translate')<br>
-                        <a href="http://mods-generator.oneskyapp.com"
+                        <a href="https://mods-generator.oneskyapp.com"
                            target="_blank"
-                           class="grey-text text-darken-1" style="text-decoration: underline;">http://mods-generator.oneskyapp.com</a>
+                           class="grey-text text-darken-1" style="text-decoration: underline;">https://mods-generator.oneskyapp.com</a>
                     </h6>
                 </div>
             </div>
